2ffbbe34d1b8b22f00137adc451879a932654378
Marvin Myers <153286517+archery-zaps@users.noreply.github.com>
2024-01-10 14:50:31 +0700

    Create transaction.cpp

---
+++ transaction.cpp
@@ -0,0 +1,4 @@
+#include "transaction.h"
+
+Transaction::Transaction(const Customer& customer, const Account& account, TransactionType type, double amount)
+    : customer(customer), account(account), type(type), amount(amount) {}
